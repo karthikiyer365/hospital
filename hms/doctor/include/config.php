80b9b884e4b88dbedbd540de38c8271a1b351fd2
karthikiyer365 <karthikiyer365@gmail.com>
2022-04-13 12:07:09 +0530

    Initial commit

---
+++ hms/doctor/include/config.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_SERVER','containers-us-west-33.railway.app');
+define('DB_USER','root');
+define('DB_PASS' ,'0GvwYDKiYtu1niX8cidA');
+define('DB_NAME', 'railway');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME, 5537);
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
